e5667e551d75845b7514b65c8ad414fca4a3ffcb
“Rubens <rubensmatheusvenancio@gmail.com>
2023-04-15 06:59:52 -0300

    agr com ejs

--- site/index.js
+++ site/index.js
@@ -1,24 +1,24 @@
 const express = require('express')
 const app = express()
 const { Atividades } = require('./models')
+const path = require('path')
+const indexRout = require('./rotas/index.rotas')
 
 app.use(express.json())
-app.use(express.static('front'))
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs')
 
-
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/front/html.html')
-});
+app.use('/', indexRout)
 
 app.get('/dados', async function(req, res) {
     const atividade = await Atividades.findAll()
     res.json({atividades: atividade})
 });
 
-app.get('/dados/:id', async (req, res) => {
-    const atividade = await Atividades.findByPk(req.params.id)
-    res.json({atividades: atividade})
-})
+// app.get('/dados/:id', async (req, res) => {
+//     const atividade = await Atividades.findByPk(req.params.id)
+//     res.json({atividades: atividade})
+// })
 
 app.post('/dados', async (req, res) => {
     const atividades = await Atividades.create(req.body)
@@ -27,16 +27,16 @@ app.post('/dados', async (req, res) => {
     res.json({atividades: atividade})
 })
 
-app.delete('/dados', async (req, res) => {
-    const id = req.query.id
-    const atividade = await Atividades.findByPk(id)
-    if (atividade){
-        await atividade.destroy()
-        res.json({msg: "Atividadet deletada com sucesso!"})
-    }else{
-        res.status(400).json({msg: "Atividade não encontrado!"})
-    }
-})
+// app.delete('/dados', async (req, res) => {
+//     const id = req.query.id
+//     const atividade = await Atividades.findByPk(id)
+//     if (atividade){
+//         await atividade.destroy()
+//         res.json({msg: "Atividadet deletada com sucesso!"})
+//     }else{
+//         res.status(400).json({msg: "Atividade não encontrado!"})
+//     }
+// })
 
 app.listen(3000, function() {
   console.log('Aplicativo da web rodando na porta 3000!');
---
+++ site/rotas/index.rotas.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = express.Router()
+const { Atividades } = require('../models')
+const moment = require('moment')
+
+moment.locale('tp-br')
+
+router.get('/', async (req, res) => {
+    const atividade = await Atividades.findAll({
+        // include: [{
+        //    model: atividade
+        // }], raw: true, nest: true
+    })
+    //console.log(atividade)
+    // console.log('\n')
+    const atvResult = atividade.map((atv) => prepararResultado(atv))
+    console.log(atvResult)
+    res.render('pages/index', {atividades: atvResult})
+
+})
+
+function prepararResultado(atv){
+
+    const result = Object.assign({}, atv)
+    result.novadata = moment(new Date(result.dataValues.data)).add(10, 'days').calendar()
+
+    if (result.createdAt) delete result.createdAt
+    if (result.updatedAt) delete result.updatedAt
+    
+    return result
+}
+
+module.exports = router
